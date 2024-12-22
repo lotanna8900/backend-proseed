c2ca975a6b702931fcc07964797a45a632297939
lotanna8900 <lotanna8900@gmail.com>
2024-12-22 11:47:03 +0100

    Add retry logic for MongoDB connection

--- config/db.js
+++ config/db.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   const uri = process.env.MONGO_URI;
   console.log(`Attempting to connect to MongoDB with URI: ${uri}`);
-  try {
-    const conn = await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 5000 // You can set other options as needed
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
-  }
+  const connectWithRetry = () => {
+    mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 })
+      .then(conn => {
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      })
+      .catch(err => {
+        console.error(`Error: ${err.message}`);
+        console.log('Retrying MongoDB connection in 5 seconds...');
+        setTimeout(connectWithRetry, 5000);
+      });
+  };
+  connectWithRetry();
 };
 
 module.exports = connectDB;
