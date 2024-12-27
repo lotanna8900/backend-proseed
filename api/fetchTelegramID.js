624f652e5a93d93e7231dde11ba8f52e2f14042a
lotanna8900 <lotanna8900@gmail.com>
2024-12-27 12:15:04 +0100

    Home segment updates

--- api/fetchTelegramID.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const telegramID = req.query.telegramId;
-  if (!telegramID) {
-    return res.status(400).json({ error: 'Missing telegramId query parameter' });
-  }
-  res.json({ telegramID });
-};
-
